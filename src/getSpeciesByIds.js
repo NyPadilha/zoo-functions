2671b2837f25431282aa729cc315e708b6a223b5
NyPadilha <47741746+NyPadilha@users.noreply.github.com>
2023-04-11 20:45:24 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  
-};
+const getSpeciesByIds = (...ids) => (!ids.length ? []
+  : species.filter((specie) => ids.includes(specie.id)));
 
 module.exports = getSpeciesByIds;
