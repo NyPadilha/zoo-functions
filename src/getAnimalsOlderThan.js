655f84fd3f838cb60cb59eb195976f230a72dba5
NyPadilha <47741746+NyPadilha@users.noreply.github.com>
2023-04-11 21:29:46 -0300

    requisito 2 (dyr = animal)

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,7 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
-};
+const getAnimalsOlderThan = (animal, age) => !species.find((specie) => specie.name
+=== animal).residents.some((dyr) => dyr.age < age);
 
 module.exports = getAnimalsOlderThan;
