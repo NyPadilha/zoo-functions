477579c07bd9df72a0f0fef721ff83392b88a928
NyPadilha <47741746+NyPadilha@users.noreply.github.com>
2023-04-12 07:54:22 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,15 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
-const isManager = (id) => {
-  // seu código aqui
-};
+const isManager = (id) => employees.map((employee) => employee.managers)
+  .reduce((acc, index) => acc.concat(index), []).some((manager) => manager === id);
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return employees.filter((employee) => employee.managers.includes(managerId))
+      .map((nome) => `${nome.firstName} ${nome.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 };
 
 module.exports = { isManager, getRelatedEmployees };
