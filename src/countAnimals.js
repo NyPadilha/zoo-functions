7ef9939e14184c9b56fcf885de8364157f824b5f
NyPadilha <47741746+NyPadilha@users.noreply.github.com>
2023-04-13 10:10:06 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,5 +1,19 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-const countAnimals = (animal) => {};
+const countAnimals = (animal) => {
+  if (!animal) {
+    return species.reduce((acc, specie) => {
+      acc[specie.name] = specie.residents.length;
+      return acc;
+    }, {});
+  }
+
+  if (Object.keys(animal).length === 1) {
+    return species.find((dyr) => dyr.name === animal.species).residents.length;
+  }
+  return species.find((dyr) => dyr.name === animal.species)
+    .residents.filter((animals) => animals.sex === animal.sex).length;
+};
 
 module.exports = countAnimals;
