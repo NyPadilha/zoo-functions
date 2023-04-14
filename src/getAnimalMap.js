179583182a2fa5a153e1fd166ddb24fa5f172eb4
NyPadilha <47741746+NyPadilha@users.noreply.github.com>
2023-04-14 11:50:22 -0300

    requisito 14(bem baguncado

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,48 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+
+const getAnimalsByLocal = (local) => species.filter((i) => i.location === local);
+
+const location = ['NE', 'NW', 'SE', 'SW'];
+
+const sortedByLocal = location.reduce((acc, curr) => {
+    acc[curr] = getAnimalsByLocal(curr).map((animal) => animal.name);
+    return acc;
+  }, {});
+
+const sortedByLocalAndName = (parameters) => location.reduce((acc, curr) => {
+  let value;
+  acc[curr] = getAnimalsByLocal(curr).map((animal) => {
+    if (!parameters) value = animal.residents.map((dyr) => dyr.name);
+    if (parameters === 'sort') value = animal.residents.map((dyr) => dyr.name).sort();
+    if (parameters === 'sexFemale') value = animal.residents.filter((dyr) => dyr.sex === 'female').map((a) => a.name);
+    if (parameters === 'sexMale') value = animal.residents.filter((dyr) => dyr.sex === 'male').map((a) => a.name);
+    if (parameters === 'sexFemaleSort') value = animal.residents.filter((dyr) => dyr.sex === 'female').map((a) => a.name).sort();
+    if (parameters === 'sexMaleSort') value = animal.residents.filter((dyr) => dyr.sex === 'male').map((a) => a.name).sort();
+
+    const animals = {};
+    animals[animal.name] = value
+    return animals;
+  });
+  return acc;
+}, {});
 
 const getAnimalMap = (options) => {
-  // seu código aqui
+  if (!options || !options.includeNames) return sortedByLocal;
+
+  if (options.sorted && options.sex === 'female') return sortedByLocalAndName('sexFemaleSort');
+  if (options.sorted && options.sex === 'male') return sortedByLocalAndName('sexMaleSort');
+  
+
+  if (options.sorted) return sortedByLocalAndName('sort');
+
+  if (options.sex === 'female') return sortedByLocalAndName('sexFemale');
+  else if (options.sex === 'male') return sortedByLocalAndName('sexMale');
+
+  if (options.includeNames) return sortedByLocalAndName();
 };
+
+console.log(getAnimalMap({ includeNames: true, sex: 'female', sorted: true }).NE[0]);
+//console.log(getAnimalsByLocal('NE').map((animal) => animal.residents.map((dyr) => dyr.name).sort()))
 
 module.exports = getAnimalMap;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,9 +1,7 @@
 const handlerElephants = require('../src/handlerElephants');
 
 describe('Testes da função HandlerElephants', () => {
-  it('', () => {
-    expect('').toBe('');
-  });
+
 });
 
 // requisito 6, 7, 15
