311c85959e2c1d76fea9d6eb5c56f742fd7d46fd
NyPadilha <47741746+NyPadilha@users.noreply.github.com>
2023-04-14 12:44:07 -0300

    lint do 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -6,43 +6,52 @@ const getAnimalsByLocal = (local) => species.filter((i) => i.location === local)
 const location = ['NE', 'NW', 'SE', 'SW'];
 
 const sortedByLocal = location.reduce((acc, curr) => {
-    acc[curr] = getAnimalsByLocal(curr).map((animal) => animal.name);
-    return acc;
-  }, {});
+  acc[curr] = getAnimalsByLocal(curr).map((animal) => animal.name);
+  return acc;
+}, {});
+
+const sortedBy = (parameters, animal) => {
+  let value;
+  if (parameters === 'onlyNames') value = animal.residents.map((dyr) => dyr.name);
+  if (parameters === 'sort') value = animal.residents.map((dyr) => dyr.name).sort();
+  if (parameters.par === 'sex') {
+    value = animal.residents.filter((dyr) => dyr.sex === parameters.sex).map((a) => a.name);
+  }
+  if (parameters.par === 'sortBySex') {
+    value = animal.residents.filter((dyr) => dyr.sex === parameters.sex).map((a) => a.name).sort();
+  }
+  return value;
+};
 
 const sortedByLocalAndName = (parameters) => location.reduce((acc, curr) => {
-  let value;
   acc[curr] = getAnimalsByLocal(curr).map((animal) => {
-    if (!parameters) value = animal.residents.map((dyr) => dyr.name);
-    if (parameters === 'sort') value = animal.residents.map((dyr) => dyr.name).sort();
-    if (parameters === 'sexFemale') value = animal.residents.filter((dyr) => dyr.sex === 'female').map((a) => a.name);
-    if (parameters === 'sexMale') value = animal.residents.filter((dyr) => dyr.sex === 'male').map((a) => a.name);
-    if (parameters === 'sexFemaleSort') value = animal.residents.filter((dyr) => dyr.sex === 'female').map((a) => a.name).sort();
-    if (parameters === 'sexMaleSort') value = animal.residents.filter((dyr) => dyr.sex === 'male').map((a) => a.name).sort();
-
     const animals = {};
-    animals[animal.name] = value
+    animals[animal.name] = sortedBy(parameters, animal);
     return animals;
   });
   return acc;
 }, {});
 
+const temSex = (options) => {
+  if (options.sorted && options.sex) {
+    return sortedByLocalAndName({ par: 'sortBySex',
+      sex: options.sex });
+  }
+  if (options.sex) return sortedByLocalAndName({ par: 'sex', sex: options.sex });
+};
+
+const temName = (options) => {
+  if (options.sex) return temSex(options);
+  if (options.sorted) return sortedByLocalAndName('sort');
+  if (options.includeNames) return sortedByLocalAndName('onlyNames');
+};
+
 const getAnimalMap = (options) => {
   if (!options || !options.includeNames) return sortedByLocal;
-
-  if (options.sorted && options.sex === 'female') return sortedByLocalAndName('sexFemaleSort');
-  if (options.sorted && options.sex === 'male') return sortedByLocalAndName('sexMaleSort');
-  
-
-  if (options.sorted) return sortedByLocalAndName('sort');
-
-  if (options.sex === 'female') return sortedByLocalAndName('sexFemale');
-  else if (options.sex === 'male') return sortedByLocalAndName('sexMale');
-
-  if (options.includeNames) return sortedByLocalAndName();
+  if (options.includeNames) return temName(options);
 };
 
 console.log(getAnimalMap({ includeNames: true, sex: 'female', sorted: true }).NE[0]);
-//console.log(getAnimalsByLocal('NE').map((animal) => animal.residents.map((dyr) => dyr.name).sort()))
+// console.log(getAnimalsByLocal('NE').map((animal) => animal.residents.map((dyr) => dyr.name).sort()))
 
 module.exports = getAnimalMap;
